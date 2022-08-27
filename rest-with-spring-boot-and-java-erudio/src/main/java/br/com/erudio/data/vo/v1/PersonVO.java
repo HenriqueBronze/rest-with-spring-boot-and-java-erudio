0119a23eb173cbe119f6fbb0f661852fb209eea3
Henrique Bronze <hbronze@gmail.com>
2022-08-27 13:04:46 -0300

    Json Properties

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/data/vo/v1/PersonVO.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/erudio/data/vo/v1/PersonVO.java
@@ -2,16 +2,23 @@ package br.com.erudio.data.vo.v1;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonPropertyOrder({"id","address","firstName","lastName","gender"})
 public class PersonVO implements Serializable {
     
     private Long id;
 
+    @JsonProperty("first_Name")
     private String firstName;
 
     private String lastName;
 
     private String address;
 
+    @JsonIgnore
     private String gender;
 
 
