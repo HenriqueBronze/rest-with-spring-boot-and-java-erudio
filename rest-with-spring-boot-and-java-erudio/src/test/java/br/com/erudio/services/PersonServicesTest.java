3546748733cb50cce3b941ebd3f41931f748c390
Henrique Bronze <hbronze@gmail.com>
2022-09-02 21:41:51 -0300

    mockito

---
+++ rest-with-spring-boot-and-java-erudio/src/test/java/br/com/erudio/services/PersonServicesTest.java
@@ -0,0 +1,82 @@
+package br.com.erudio.services;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import br.com.erudio.model.Person;
+import br.com.erudio.respositories.PersonRepository;
+import br.com.erudio.restwithspringbootandjavaerudio.unittests.mapper.mocks.MockPerson;
+
+@TestInstance(Lifecycle.PER_CLASS)
+@ExtendWith(MockitoExtension.class)
+public class PersonServicesTest {
+
+    MockPerson input;
+
+    @InjectMocks
+    private PersonServices service;
+
+    @Mock
+    PersonRepository repository;
+
+    @BeforeEach
+    void setUpMocks() throws Exception{
+
+        input  = new MockPerson();
+        MockitoAnnotations.openMocks(this);
+    }
+
+
+    
+    @Test
+    void testCreate() {
+
+    }
+
+    @Test
+    void testDelete() {
+
+    }
+
+    @Test
+    void testFindAll() {
+
+    }
+
+    @Test
+    void testFindById() {
+
+        Person person = input.mockEntity();
+        person.setId(1L);
+        when(repository.findById(1L)).thenReturn(Optional.of(person));
+
+        var result = service.findById(1L);
+        assertNotNull(result);
+        assertNotNull(result.getKey());
+        assertNotNull(result.getLinks());
+        System.out.println(result.toString());
+        assertTrue(result.toString().contains("links: [</api/person/v1/1>;rel=\"self\"]"));
+
+
+    }
+
+    @Test
+    void testUpdate() {
+
+    }
+}
